5fbf8e25287b6878c1abeb9de27749a4ce69b67f
viniciusg20 <viniciusg10@hotmail.com>
2022-10-02 23:34:57 -0300

    exceção para CPF e EMAIL já cadastrados

--- src/main/java/com/example/newton/repositories/PessoaRepository.java
+++ src/main/java/com/example/newton/repositories/PessoaRepository.java
@@ -1,9 +1,14 @@
 package com.example.newton.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.newton.domain.Pessoa;
 
 public interface PessoaRepository extends JpaRepository<Pessoa, Integer>{
+	
+	Optional<Pessoa>findByCpf(String Cpf);
+	Optional<Pessoa>findByEmail(String Cpf);
 
 }
