b10a158f62083109c73adb22b6fadd2ccbc353a2
viniciusg20 <viniciusg10@hotmail.com>
2022-10-04 15:49:58 -0300

    delete para Tecnico implementado

--- src/main/java/com/example/newton/services/TecnicoService.java
+++ src/main/java/com/example/newton/services/TecnicoService.java
@@ -48,6 +48,16 @@ public class TecnicoService {
 		oldObj = new Tecnico(objDTO);
 		return repository.save(oldObj);
 	}
+	    
+	    public void delete(Integer id) {
+			Tecnico obj = findById(id);
+
+			if (obj.getChamados().size() > 0) {
+				throw new DataIntegrityViolationException("Técnico possui ordens de serviço e não pode ser deletado!");
+			}
+
+			repository.deleteById(id);
+		}
 
 		private void validaPorCpfEmail(TecnicoDTO objDTO) {
 		Optional<Pessoa> obj = pessoaRepository.findByCpf(objDTO.getCpf());
@@ -60,6 +70,8 @@ public class TecnicoService {
 			throw new DataIntegrityViolationException("E-mail já cadastrado no sistema!");
 		}
 	}
+
+		
 
 	
 
