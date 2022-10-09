a663af02473cb0819e351f82bf3e76acc28aacd4
viniciusg20 <viniciusg10@hotmail.com>
2022-10-09 20:09:44 -0300

    autorizacao com JWT

--- src/main/java/com/example/newton/config/SecurityConfig.java
+++ src/main/java/com/example/newton/config/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.env.Environment;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -17,9 +18,11 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.example.newton.security.JWTAuthenticationFilter;
+import com.example.newton.security.JWTAuthorizationFilter;
 import com.example.newton.security.JWTUtil;
 
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter{
 	
 	private static final String[]  PUBLIC_MATCHERS = {"/h2-console/**"};//permite acesso a todas as requisições que vierem depois de h2-console
@@ -40,6 +43,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 		//vai subir a configuração cors | 
 		http.cors().and().csrf().disable();  //<- desabilitar proteção contra o ataque csrf (ataque baseado em armazenamento de exceções de usuário)
 		http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
+		http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil, userDetailsService));
 		http.authorizeRequests()
 		    .antMatchers(PUBLIC_MATCHERS).permitAll()//permite acesso a todas as requisições que vierem depois de h2-console
 		    .anyRequest().authenticated();
